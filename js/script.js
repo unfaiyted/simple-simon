9ac87fb13a26cf1c6314fb1e2a05f8f2648cd78a
unfaiyted <unfaiyted@gmail.com>
2018-03-22 16:54:28 -0500

    initial functionality, able to preform gameover and restart functionality.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 
 
 var userArray = [];
-var simonArray = []
+var simonArray = [];
 var currentRound = 0;
 var roundActive = false;
 
 $('.semi-circle').hover(function () {
-    $(this).addClass('light');
+    if(roundActive === true) { $(this).addClass('light'); }
 }, function () {
-    $(this).removeClass('light');
+    if(roundActive === true) {  $(this).removeClass('light'); }
 });
 
 
@@ -41,8 +41,6 @@ $('#start-btn').click(function () {
     $('#start-btn').prop('disabled', true);
     $('#start-btn').css('background-color','red');
 
-    currentRound = 1;
-
     var timer = setInterval(function(){
         $('#cdTimer').removeClass('hide');
         $("#cdTimer").html(function(i,html){
@@ -69,23 +67,90 @@ $('#start-btn').click(function () {
 });
 
 
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // begin next round
 function nextLevel() {
+
+    currentRound++;
+    roundActive = false;
+    userArray = [];
+
+    var intervalCount = 0;
 
     $('#simon-counter').text(currentRound);
 
     var timer = setInterval(function() {
 
+        var randomInt = getRandomInt(1,4);
+        if(intervalCount <= currentRound) {
+
+            if(simonArray.length < intervalCount) {
+                switch (randomInt) {
+                    case 1:
+                        simonArray.push('red');
+                        $('#red').addClass('light');
+
+                        break;
+                    case 2:
+                        simonArray.push('green');
+                        $('#green').addClass('light');
+                        break;
+                    case 3:
+                        simonArray.push('blue');
+                        $('#blue').addClass('light');
+                        break;
+                    case 4:
+                        simonArray.push('yellow');
+                        $('#yellow').addClass('light');
+                        break;
+                }
+            } else {
+                $('#' + simonArray[intervalCount]).addClass('light');
+            }
+
+            setTimeout(function() {
+                $('.semi-circle').removeClass('light');
+            }, 400);
 
 
-    })
+            intervalCount++;
+        } else {
+            roundActive = true;
+            clearTimeout(timer);
+        }
+
+    }, 1250);
 
 
 
 }
+
+
+$('#retry').click(function () {
+    $('#start-btn').prop('disabled', false);
+    $('#start-btn').css('background-color','green');
+    $('#simon-counter').text("0")
+    $('#gameOver').addClass('hide');
+
+});
 
 // reset game settings, display GAME OVER TEXT
 // high score screen ?? eventually (local storage)
 function gameOver() {
 
+    $('#go-score').text($('#simon-counter').text());
+
+
+    roundActive = false;
+    userArray = [];
+    simonArray = [];
+    currentRound = 0;
+    $('#gameOver').removeClass('hide');
 }
+
+
+
